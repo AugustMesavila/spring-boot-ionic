9ffaa617010277d6a5d6de7ed4cda24b58148afe
Augusto Mesavila <augusto_mesavila@hotmail.com>
2020-10-27 16:36:14 -0300

    Project Start

---
+++ src/test/java/com/mesavila/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesavila.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
