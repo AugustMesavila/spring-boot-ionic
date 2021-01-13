842906d1da461c462004f86358c0cb2162a7d9cc
Augusto Mesavila <augusto_mesavila@hotmail.com>
2021-01-12 22:30:12 -0300

    Product and Category relationship

--- src/main/java/com/mesavila/cursomc/CursomcApplication.java
+++ src/main/java/com/mesavila/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mesavila.cursomc.domain.Category;
+import com.mesavila.cursomc.domain.Product;
 import com.mesavila.cursomc.repositories.CategoryRepository;
+import com.mesavila.cursomc.repositories.ProductRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
+
 	@Autowired
-	private CategoryRepository repository;
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,12 +26,24 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
+
+		Product prod1 = new Product(null, "Computador", 2000.00);
+		Product prod2 = new Product(null, "Impressora", 800.00);
+		Product prod3 = new Product(null, "Mouse", 80.00);
+
+		cat1.getProducts().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat1.getProducts().addAll(Arrays.asList(prod2));
 		
-		repository.saveAll(Arrays.asList(cat1,cat2));
+		prod1.getCategories().addAll(Arrays.asList(cat1));
+		prod2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategories().addAll(Arrays.asList(cat1));
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+
 	}
 
 }
--- src/main/java/com/mesavila/cursomc/domain/Product.java
+++ src/main/java/com/mesavila/cursomc/domain/Product.java
@@ -8,10 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable {
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -19,18 +21,23 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
+	
+	@ManyToMany
+	@JoinTable(name = "PRODUCT_CATEGORY_REL",
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id")
+	)
+	private List<Category> categories = new ArrayList<>();
 
-	@ManyToMany(mappedBy = "categories")
-	private List<Product> products = new ArrayList<>();
-
-	public Category() {
-
+	public Product() {
 	}
 
-	public Category(Integer id, String name) {
+	public Product(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -49,20 +56,28 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-	}
-
-	public List<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(List<Product> products) {
-		this.products = products;
 	}
 
 	@Override
@@ -73,7 +88,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
